f7315e3b9108ba3231f422b632d436b697e21ff1
Phil Schutte <schuttepm@gmail.com>
2020-04-04 14:11:28 -0400

    refactoring v2

--- index.js
+++ index.js
@@ -18,96 +18,26 @@ client.on('message', msg => {
     if (msg.content.substring(0,1) === '!') {
         let args = msg.content.substring(1).split(' ');
         let cmd = args[0].toLowerCase();
+        let voiceClip = Boolean(utils.getVC(cmd));
         let id = msg.member.voice.channelID;
         let vc = client.channels.cache.get(id);
         args = args.splice(1);
+        if (Boolean(voiceClip)) {
+            if (Boolean(vc)) {
+                vc.join().then(connection => {
+                    const dispatcher = connection.play(`${voiceClip}`);
+                    dispatcher.on("end", end => {});
+                }).catch(err => {
+                    console.log(err);
+                });
+                setTimeout(() => vc.leave(), 5 * 60 * 1000);
+            }
+        }
         switch(cmd) {
             case 'pathetic':
                 msg.channel.send('', {files: utils.pathetic()});
                 break;
-            case 'love':
-            case 'rkami':
-            case 'jmo':
-            case 'wake':
-            case 'bar':
-            case 'pizza':
-            case 'shipment':
-            case 'shitment':
-            case 'bargin':
-            case 'munchies':
-            case 'midi':
-            case 'skin':
-            case 'heed':
-            case 'art':
-            case 'mcguffin':
-            case 'fanfare':
-            case 'rva':
-            case 'chinmad':
-            case 'chins':
-            case 'dexter':
-            case 'larry':
-            case 'washington':
-            case 'bawls':
-            case 'bawls2':
-            case 'padme':
-            case 'phil':
-            case 'skin2':
-            case 'wdeg':
-            case 'thezone':
-            case 'singularity1':
-            case 'xigbe2':
-            case 'vip':
-            case 'vip2':
-            case 'deal':
-            case 'runes':
-            case 'arc':
-            case 'babytime':
-            case 'dancefight2':
-            case 'yeahboi1':
-            case 'wtf':
-            case 'jotaro':
-            case 'wow':
-            case 'amnesia':
-            case 'hooligan':
-            case 'jims':
-            case 'sand':
-            case 'shoes1':
-            case 'runes2':
-            case 'shoes3':
-            case 'shoes5':
-            case 'jedi':
-            case 'bickering':
-            case 'duke':
-            case 'egypt':
-            case 'nudes':
-            case 'owen':
-            case 'alone':
-            case 'friend':
-            case 'steve':
-            case 'lemonade':
-            case 'whatsup':
-            case 'weekend':
-            case 'beautiful':
-            case 'longbeautiful':
-            case 'cheese':
-            case 'yes':
-            case 'fun':
-            case 'larry2':
-            case 'ass':
-            case 'bet':
-            case 'lasermoon':
-            case 'bonjo':
-            case 'icu':    
-                if (Boolean(vc)) {
-                    vc.join().then(connection => {
-                        const dispatcher = connection.play(`./assets/audio/${cmd}.wav`);
-                        dispatcher.on("end", end => {});
-                    }).catch(err => {
-                        console.log(err);
-                    });
-                    setTimeout(() => vc.leave(), 5 * 60 * 1000);
-                }
-                break;
+
         }
     }
 })
